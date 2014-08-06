02b3d3f4b76111fcb4e2b434b1e9cc1241ce8c64
Jens Krause <jens@propertyshelf.com>
2014-08-06 18:07:37 +0700

    stable theme from mls-ecuador.com including responsive Development Gallery

---
+++ javascript/move-porlet-logo.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $(".agency_logo").insertAfter(".contact_details");
+    $("body.section-new-developments #portal-column-content" ).insertAfter("body.section-new-developments #portal-column-one" );
+    $("body.section-urbanizaciones-nuevas #portal-column-content" ).insertAfter("body.section-urbanizaciones-nuevas #portal-column-one" );
+});
